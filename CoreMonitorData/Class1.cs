4bc808cd509c89ec4c93032e515bc5ae7e61dbd7
Michaelmilk <gjx12358@gmail.com>
2017-02-19 22:48:20 +0800

    add data, service and workflow project

---
+++ CoreMonitorData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreMonitorData
+{
+    public class Class1
+    {
+    }
+}
